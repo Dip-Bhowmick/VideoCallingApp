7d014caecfee81b8e355403112de9de936ded31d
ITzDip <62492915+Dip-Bhowmick@users.noreply.github.com>
2020-12-18 21:40:00 +0530

    Add files via upload

---
+++ public/client.js
@@ -0,0 +1,57 @@
+const socket = io()
+const peers = {}
+const Room = new URLSearchParams(window.location.search).get('room')
+
+socket.on('InitPeer',PeerOption=>{
+    window.myPeer = new Peer(undefined, PeerOption);
+    myPeer.on('open', id => {
+        console.log("hi")
+        socket.emit('JoinRoom', Room, id)
+    })
+});
+
+
+navigator.mediaDevices.getUserMedia({video: true,audio: true}).then(stream => {
+    
+    const myVideo = document.createElement('video')
+    myVideo.muted = true
+    myVideo.srcObject = stream
+    myVideo.addEventListener('loadedmetadata', () => {
+    myVideo.play()
+    })
+    document.getElementById('video-grid').append(myVideo)
+ 
+  
+    myPeer.on('call', call => {
+      call.answer(stream)
+      const video = document.createElement('video')
+      call.on('stream', userVideoStream => {
+        video.srcObject = userVideoStream
+        video.addEventListener('loadedmetadata', () => {
+        video.play()
+        })
+        document.getElementById('video-grid').append(video)
+      })
+    })
+  
+    socket.on('NewUser',id => {
+        const call = myPeer.call(id, stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            video.srcObject = userVideoStream
+            video.addEventListener('loadedmetadata', () => {
+            video.play()
+            })
+            document.getElementById('video-grid').append(video)
+        })
+        call.on('close', () => {
+          video.remove()
+        })
+      
+        peers[userId] = call
+    })
+})
+
+socket.on('UserDisconnected', id => {
+    if (peers[id]) peers[id].close()
+})
