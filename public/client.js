12e44c87b1a97c7accc50a7f69d7f527feeb47c0
ITzDip <62492915+Dip-Bhowmick@users.noreply.github.com>
2020-12-21 11:05:36 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -7,8 +7,7 @@ const peerOption = {
     path : '/PeerServer',
     host : window.location.host,
 }
-if (parseInt(window.location.port)) peerOption["port"] = parseInt(window.location.port)
-
+//if (parseInt(window.location.port)) peerOption["port"] = parseInt(window.location.port)
 const myPeer = new Peer(null,peerOption)
 myPeer.on('open', id => {
     socket.emit('JoinRoom', Room, id)
